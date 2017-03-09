380502827129402c66e8f0e0c588c6ea05148bfb
George Fawcett <georgealanfawcett@gmail.com>
2017-03-09 17:00:38 +0000

    Custom array mapper

---
+++ arraymapcallback.js
@@ -0,0 +1,10 @@
+var words = ["ground", "control", "to", "major", "tom"];
+
+var wordLengths = words.map(function(word) {
+  return word.length;
+});
+
+console.log(wordLengths);
+
+// example result
+// [6, 7, 2, 5, 3]
